bc3d31a340ec7d2d46d68cdb49fb28f7eba6a755
palomitadkaramelo <palomitadkaramelo@gmail.com>
2026-04-24 14:13:06 -0600

    Update Programa4.c

--- Programa4.c
+++ Programa4.c
@@ -2,7 +2,24 @@
 void main ()
 {
   int i,j;
-  int matriz [2][2]=({1,1},{1,1});
+  int m[2][2];
+
+for (int i=0; i<2; i++)
+{
+  for (int j=0; j<3; j++)
+{
+  m(i)(j)=1
+    }
+}
+printf(la matriz original es
+for(int j=0; j<2; j++)
+{
+ if (i==j)
+{ m(i)(j)=0
+  }
+else
+{ m
+  { printf
   printf ("tabla, cuadrado");
   for (int=0, i<2; i++)
     {
