1105060b198cc9b999b200414d3ec884d4662caf
palomitadkaramelo <palomitadkaramelo@gmail.com>
2026-04-24 14:02:52 -0600

    Create Programa4.c

---
+++ Programa4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main ()
+{
+  int i,j;
+  int matriz [2][2]=({1,1},{1,1});
+  printf ("tabla, cuadrado");
+  for (int=0, i<2; i++)
+    {
+      matriz[i][i]=0
+    }
+  for (int=0; i<2; i++)
+    { 
+    for (int=0; j<2; j++)
+    }
+    printf("%d", matriz[i][j]);
+
